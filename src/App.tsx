5c221fe2e13ef5b28eb2fb0a5d8922153df9c38e
akm <akm2000@gmail.com>
2021-07-19 15:51:14 +0900

    :sparkles: OpenSeadragonを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import OpenSeadragon from 'openseadragon';
+import React, {useEffect} from 'react';
 import './App.css';
 
+const styles = {
+  viewer: {
+    width: '800px',
+    height: '600px',
+  },
+};
+
 function App() {
+  useEffect(() => {
+    OpenSeadragon({
+      id: 'osd-viewer',
+      prefixUrl: '/images/',
+      tileSources: '/tiles/tree1.dzi',
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="osd-viewer" style={styles.viewer}></div>
       </header>
     </div>
   );
